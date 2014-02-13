b49c1e7632b0daa8a182858eab30044215391418
Paul Weingardt <weingardt.dev@googlemail.com>
2014-02-13 17:45:32 +0100

    added local storage support for user created checkgroups, added leanmodal

--- kspchecklist.js
+++ kspchecklist.js
@@ -1,11 +1,14 @@
 
+var userGroups = new Array();
 
 $(function()  {
+	$('a[rel*=leanModal]').leanModal({ top : 200, overlay : 0.4, closeButton: ".modal_close" });
+	
 	$("#create_button").click(function() {
 
 		var inputs = new Array();
 		
-		$('#mission_form input:checkbox:checked').each(function() {
+		$('input:checkbox:checked.checkgroup').each(function() {
 			inputs.push(this.name);
 		});
 		var groups = createCheckList(inputs);
@@ -30,8 +33,130 @@ $(function()  {
 			div.append(divc);
 		}
 	});
+	
+	$("#create_check_group").click(function() {
+		$("#btn_create_check_group").click();
+		$("#group_items").empty();
+		$("#input_group_name").val("");
+		$("#add_item").click();
+		$("#create_done").html("Create").data("groupindex", -1);
+	});
+	
+	$("#add_item").click(function() {
+		$("#group_items").append(createItem(""));
+	});
+	
+	$("#create_done").click(function() {
+		var name = $("#input_group_name").val();
+		var groupindex = $(this).data("groupindex");
+		
+		var group = new Group(name);
+		
+		$('input:text.creategroup').each(function() {
+			var item = $(this).val();
+			if(item != "") {
+				group.add($(this).val());
+			}
+		});
+		
+		if(groupindex == -1) {
+			userGroups.push(group);
+		} else {
+			userGroups[groupindex] = group;
+		}
+		
+		saveGroups();
+		updateUserGroups();
+		$(".modal_close").click();
+	});
+	
+	updateUserGroups();
 });
 
+updateUserGroups = function() {
+	if(localStorage) {
+		userGroups = JSON.parse(localStorage.getItem("usergroups"));
+	}
+	
+	$("#user_groups").empty();
+	
+	for(var i in userGroups) {
+		var g = userGroups[i];
+		var l = $("<label />").addClass("checkbox");
+		var input = $("<input />", {
+			type: "checkbox",
+			name: g.name
+		}).addClass("checkgroup");
+		l.append(input).append(g.name);
+		
+		var ae = $("<a />").addClass("edit").data("groupname", g.name);
+		ae.click(editCheckGroup);
+	
+		l.append(ae);
+		
+		ae = $("<a />").addClass("delete").data("groupname", g.name);
+		ae.click(deleteCheckGroup);
+		
+		l.append(ae);
+		
+		$("#user_groups").append(l);
+	}
+};
+
+editCheckGroup = function() {
+	var g = searchGroup($(this).data("groupname"));
+	$("#btn_create_check_group").click();
+	$("#group_items").empty();
+	$("#input_group_name").val(g.name);
+	$("#create_done").html("Edit");
+	
+	for(var i in g.items) {
+		$("#group_items").append(createItem(g.items[i]));
+	}
+	
+	$("#create_done").data("groupindex", searchGroupIndex(g.name));
+};
+
+deleteCheckGroup = function() {
+	var g = searchGroup($(this).data("groupname"));
+	var i = userGroups.indexOf(g);
+	userGroups.splice(i, 1);
+	
+	saveGroups();
+	updateUserGroups();
+};
+
+saveGroups = function() {
+	if(localStorage) {
+		localStorage.setItem('usergroups', JSON.stringify(userGroups));
+	}
+};
+
+remove_item = function() {
+	$(this).parent().parent().remove();
+};
+
+createItem = function(name) {
+	var d = $('<div></div>').addClass("form-group");
+	var d2 = $("<div></div>").addClass("col-sm-offset-2").addClass("col-sm-6");
+	var input = $("<input />", {
+		placeholder: "Item",
+		type: "text"
+	}).addClass("form-control").addClass("creategroup");
+	input.val(name);
+	d2.append(input);
+	
+	d.append(d2);
+	
+	d2 = $("<div></div>").addClass("col-sm-2");
+	var btn = $("<button/>", {
+		click: remove_item
+	}).addClass("btn").addClass("btn-default").addClass("btn_full").append("remove");
+	d2.append(btn);
+	d.append(d2);
+	
+	return d;
+};
 
 createCheckList = function(values) {
 	var groups = new Array();
@@ -89,7 +214,15 @@ createCheckList = function(values) {
 			g.add("Monopropellant");
 			g.add("RCS thrusters");
 			g.add("Docking port (correct orientation)");
-			break;			
+			break;
+			
+		default:
+			var ug = searchGroup(name);
+			g.name = ug.name;
+			for(var i in ug.items) {
+				g.add(ug.items[i]);
+			}
+			break;
 		}
 		
 		groups.push(g);
@@ -112,6 +245,26 @@ createCheckList = function(values) {
 	});
 	
 	return groups;
+};
+
+searchGroup = function(name) {
+	for(var i in userGroups) {
+		var g = userGroups[i];
+		if(g.name == name) {
+			return g;
+		}
+	}
+	return new Group("undefined");
+};
+
+searchGroupIndex = function(name) {
+	for(var i in userGroups) {
+		var g = userGroups[i];
+		if(g.name == name) {
+			return i;
+		}
+	}
+	return -1;
 };
 
 
