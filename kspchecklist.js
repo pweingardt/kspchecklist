2ef41247d165c361469abc6c88c959066a9a54fb
Paul Weingardt <weingardt.dev@googlemail.com>
2014-02-14 00:07:40 +0100

    moved code into checkgroups.js, added jquery validator

--- kspchecklist.js
+++ kspchecklist.js
@@ -2,6 +2,51 @@
 var userGroups = new Array();
 
 $(function()  {
+	jQuery.validator.setDefaults({
+		errorPlacement : function(error, element) {
+
+			if (element.parent().hasClass('input-prepend')
+					|| element.parent().hasClass('input-append')) {
+				error.insertAfter(element.parent());
+			} else {
+				error.insertAfter(element);
+			}
+		},
+		errorElement : "small",
+		wrapper : "div",
+		highlight : function(element) {
+			$(element).closest('.form-group').addClass('has-error');
+		},
+		success : function(element) {
+			$(element).closest('.form-group').removeClass('has-error');
+		}
+	});
+	
+	jQuery.validator.addMethod("uniqueName", function(value, element) {
+		var curindex = $("#create_done").data("groupindex");
+		if(curindex == -1) {
+			return searchGroupIndex(value) == -1;
+		} else {
+			return curindex == searchGroupIndex(value) ||  searchGroupIndex(value) == -1;
+		}
+	});
+	
+	jQuery.validator.addMethod("uniqueItem", function(value, element) {
+		if(value == "") {
+			return true;
+		}
+		
+		var count = 0;
+		$('input:text.creategroup').each(function() {
+			var item = $(this).val();
+			if(item == value) {
+				++count;
+			}
+		});
+		
+		return count <= 1;
+	});
+	
 	$('a[rel*=leanModal]').leanModal({ top : 200, overlay : 0.4, closeButton: ".modal_close" });
 	
 	$("#create_button").click(function() {
@@ -47,6 +92,10 @@ $(function()  {
 	});
 	
 	$("#create_done").click(function() {
+		if(!$("#create_form").valid()) {
+			return;
+		}
+		
 		var name = $("#input_group_name").val();
 		var groupindex = $(this).data("groupindex");
 		
@@ -68,6 +117,17 @@ $(function()  {
 		saveGroups();
 		updateUserGroups();
 		$(".modal_close").click();
+	});
+	
+	$("#create_form").validate({
+		rules: {
+			group_name: { uniqueName : true, required: true },
+			item : {uniqueItem : true}
+		},
+		messages : {
+			group_name: { uniqueName: "Groupname not unique", required: "* required"},
+			item : {uniqueItem: "Item does already exist in this group"}
+		}
 	});
 	
 	updateUserGroups();
@@ -121,21 +181,6 @@ editCheckGroup = function() {
 	$("#create_done").data("groupindex", searchGroupIndex(g.name));
 };
 
-deleteCheckGroup = function() {
-	var g = searchGroup($(this).data("groupname"));
-	var i = userGroups.indexOf(g);
-	userGroups.splice(i, 1);
-	
-	saveGroups();
-	updateUserGroups();
-};
-
-saveGroups = function() {
-	if(localStorage) {
-		localStorage.setItem('usergroups', JSON.stringify(userGroups));
-	}
-};
-
 remove_item = function() {
 	$(this).parent().parent().remove();
 };
@@ -145,7 +190,8 @@ createItem = function(name) {
 	var d2 = $("<div></div>").addClass("col-sm-offset-2").addClass("col-sm-6");
 	var input = $("<input />", {
 		placeholder: "Item",
-		type: "text"
+		type: "text",
+		name: "item"
 	}).addClass("form-control").addClass("creategroup");
 	input.val(name);
 	d2.append(input);
@@ -162,122 +208,3 @@ createItem = function(name) {
 	return d;
 };
 
-createCheckList = function(values) {
-	var groups = new Array();
-	
-	for(var v in values) {
-		var name = values[v];
-		
-		var g = new Group("(undefined)");
-		
-		switch(name) {
-		case "LANDING":
-			g.name = "(Atmospheric) landing";
-			g.add("Dragchutes (atmospheric)");
-			g.add("Parachutes (atmospheric)");
-			g.add("Legs (long enough)");
-			g.add("Ladder for EVA (if needed)");
-			break;
-			
-		case "ORBITING":
-			g.name = "Orbiting";
-			break;
-			
-		case "KETHANE_MINING":
-			g.name = "Kethane mining";
-			g.add("Kethane container");
-			g.add("Kethane converter");
-			g.add("Kethane mining drill");
-			g.add("Kethane scanner");
-			break;
-			
-		case "SCIENCING":
-			g.name = "Sciencing";
-			g.add("Communication link to Kerbin");
-			
-			g.add("Gravman Negative Gravioli Detector");
-			g.add("2HOT Thermometer");
-			g.add("PresMat Barometer");
-			g.add("Double-C Seismic Accelerometer");
-			
-			g.add("SC-9001 Science Junior");
-			g.add("Mystery Goo Containment Unit");
-			g.add("Mobile processing Lab MPL-LG-2");
-			g.add("Sensor array computing nose cone");
-			break;
-			
-		case "REMOTE_TECHING":
-			g.name = "Remote teching";
-			g.add("Solar panels (big enough)");
-			g.add("Short range communication antenna");
-			g.add("Long range communication dish");
-			break;
-			
-		case "DOCKING":
-			g.name = "Docking";
-			g.add("Monopropellant");
-			g.add("RCS thrusters");
-			g.add("Docking port (correct orientation)");
-			break;
-			
-		default:
-			var ug = searchGroup(name);
-			g.name = ug.name;
-			for(var i in ug.items) {
-				g.add(ug.items[i]);
-			}
-			break;
-		}
-		
-		groups.push(g);
-	}
-	
-	var g = new Group("General");
-	g.add("Hatch clear for EVA");
-	g.add("Lights");
-	g.add("Reaction wheels (if necessary)");
-	groups.push(g);
-	
-	g = new Group("Energy");
-	g.add("Solar panels");
-	g.add("Batteries");
-	g.add("Back-up generator");
-	groups.push(g);
-	
-	groups.sort(function(a, b) {
-		return (a.items.length < b.items.length);
-	});
-	
-	return groups;
-};
-
-searchGroup = function(name) {
-	for(var i in userGroups) {
-		var g = userGroups[i];
-		if(g.name == name) {
-			return g;
-		}
-	}
-	return new Group("undefined");
-};
-
-searchGroupIndex = function(name) {
-	for(var i in userGroups) {
-		var g = userGroups[i];
-		if(g.name == name) {
-			return i;
-		}
-	}
-	return -1;
-};
-
-
-function Group(n) {
-	this.name = n;
-	this.items = new Array();
-	
-	this.add = function(name) {
-		this.items.push(name);
-	};
-	
-}
